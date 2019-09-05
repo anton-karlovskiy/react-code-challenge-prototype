dee8c393ffb9e7ea2d4ae8f57d0beb75d38c1830
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-09-05 15:31:53 +0300

    confirmed sagas workflow

--- src/components/Spin/index.js
+++ src/components/Spin/index.js
@@ -1,16 +1,21 @@
 
 import React from 'react';
 
+// action dispatches
 import { increment, decrement } from '../../actions/spin';
+// sagas
+import { doubleIncrement, doubleDecrement } from '../../actions/doubleSpin';
 
 import { connectTo } from '../../utils/generic';
 
-const Spin = ({ increment, decrement, spinNumber }) => {
+const Spin = ({ increment, decrement, spinNumber, doubleIncrement, doubleDecrement }) => {
   return (
     <div>
       <h1>{spinNumber}</h1>
       <button onClick={() => increment()}>Increment</button>
       <button onClick={() => decrement()}>Decrement</button>
+      <button onClick={() => doubleIncrement()}>Double Increment</button>
+      <button onClick={() => doubleDecrement()}>Double Decrement</button>
     </div>
   )
 };
@@ -21,7 +26,9 @@ const mapStateToProps = state => ({
 
 const mapActionsToProps = {
   increment,
-  decrement
+  decrement,
+  doubleIncrement,
+  doubleDecrement
 };
 
 export default connectTo(mapStateToProps, mapActionsToProps, Spin);
