30e75ec3fb09cc87b06014b0cbaa1ca1c891aa90
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-09-05 23:45:01 +0300

    removed unused actions

--- src/actions/spin.js
+++
@@ -1,6 +0,0 @@
-
-import { createAction } from 'redux-act';
-
-// Create an action creator (description is optional)
-export const increment = createAction('increment the state');
-export const decrement = createAction('decrement the state');
